0251a0791f7b0f0deb26ed22cd6e0893653d99fa
amgdev2020 <jeamaroc@gmail.com>
2024-12-11 15:56:32 +0100

    Update script.php

--- script.php
+++ script.php
@@ -5,37 +5,37 @@ $type = $_GET['type'];
 $input = $_GET['input'];
 switch($type){
     case 'search':
-        $command = escapeshellcmd('AresManaga/search.py -u ' . $input);
+        $command = escapeshellcmd('search.py -u ' . $input);
         $output = shell_exec($command);
         echo $output;
         break;
     case 'home': 
-        $command = escapeshellcmd('AresManaga/home.py');
+        $command = escapeshellcmd('home.py');
         $output = shell_exec($command);
         echo $output;
         break;
     case 'manga': 
-        $command = escapeshellcmd('AresManaga/manga.py -u ' . $input);
+        $command = escapeshellcmd('manga.py -u ' . $input);
         $output = shell_exec($command);
         echo $output;
         break;
     case 'last_updates': 
-        $command = escapeshellcmd('AresManaga/last_update.py');
+        $command = escapeshellcmd('last_update.py');
         $output = shell_exec($command);
         echo $output;
         break;
     case 'series': 
-        $command = escapeshellcmd('AresManaga/series.py');
+        $command = escapeshellcmd('series.py');
         $output = shell_exec($command);
         echo $output;
         break;
     case 'chapters': 
-        $command = escapeshellcmd('AresManaga/chapters.py -u ' . $input);
+        $command = escapeshellcmd('chapters.py -u ' . $input);
         $output = shell_exec($command);
         echo $output;
         break;
     case 'home_page': 
-        $command = escapeshellcmd('AresManaga/home_page.py -u ' . $input);
+        $command = escapeshellcmd('home_page.py -u ' . $input);
         $output = shell_exec($command);
         echo $output;
         break;
